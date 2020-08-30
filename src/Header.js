05da2c314739c6ba123c4fe2b7c29c2c80eacc9e
rishichalla67 <rishichalla@ufl.edu>
2020-08-29 21:15:50 -0400

    final (non functional)

--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,35 @@
-import React from "react";
+import React from 'react'
 import './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
-import LanguageIcon from '@material-ui/icons/Language';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import {Avatar} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-export default function Header() {
-    return(
+function Header() {
+    return (
         <div className='header'>
-            <img className='header_icon'
-                 src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-                 alt=""
-            />
-
-            <div className='header_center'>
-                <input type="text"/>
+            <Link to='/'>
+                <img
+                    className="header__icon"
+                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                    alt=""
+                />
+            </Link>
+           
+            <div className='header__center'>
+                <input type="text" />
                 <SearchIcon />
             </div>
 
-            <div className="header_right">
-                <p>Become a Host</p>
-                <LanguageIcon/>
-                <ExpandMoreIcon/>
-                <Avatar/>
+            <div className='header__right'>
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
             </div>
         </div>
-    );
+    )
 }
+
+export default Header
